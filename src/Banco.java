6d8edc3013257e37156d1dbbb2eb6d7b8ac8a80c
eduarda <contato.eduarda.nd@gmail.com>
2024-06-22 16:43:32 -0300

    primeiro commit

--- src/Banco.java
+++ src/Banco.java
@@ -1,24 +1,48 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
-	private String nome;
-	private List<Conta> contas;
+    private String nome;
+    private List<Conta> contas;
 
-	public String getNome() {
-		return nome;
-	}
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public List<Conta> getContas() {
-		return contas;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
+    public List<Conta> getContas() {
+        return contas;
+    }
 
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public void removerConta(Conta conta) {
+        contas.remove(conta);
+    }
+
+    public void listarContas() {
+        System.out.println("Contas do Banco " + nome + ":");
+        for (Conta conta : contas) {
+            System.out.println("Número da Conta: " + conta.getNumero() + ", Saldo: " + conta.getSaldo());
+        }
+    }
+
+    public double calcularSaldoTotal() {
+        double saldoTotal = 0;
+        for (Conta conta : contas) {
+            saldoTotal += conta.getSaldo();
+        }
+        return saldoTotal;
+    }
 }
